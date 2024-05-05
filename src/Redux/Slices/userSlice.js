d6666e91ed1b7a0966800ef561d7a6b3a0657faf
Shanto M Rozario <shantoxrozario@gmail.com>
2024-05-05 13:57:18 +0600

    Client added

---
+++ src/Redux/Slices/userSlice.js
@@ -0,0 +1,19 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+
+    name: "user",
+    initialState:{
+        userInfo: localStorage.getItem('user')?JSON.parse(localStorage.getItem('user')):null
+    },
+    reducers:{
+        login:(state,action)=>{
+            state.userInfo = action.payload
+        }
+    }
+
+})
+
+export const {login} = userSlice.actions
+export default userSlice.reducer
